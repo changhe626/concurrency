38639ec94f928342b793bc5a72954a1eaca431f4
zhangke <changhe626@163.com>
2018-12-16 10:46:01 +0800

    ReentrantLocak 2018年12月16日10:45:54

---
+++ src/main/java/com/onyx/concurrency/example/lock/LockDemo2.java
@@ -0,0 +1,59 @@
+package com.onyx.concurrency.example.lock;
+
+import com.onyx.concurrency.annotaions.ThreadSafe;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+
+@ThreadSafe
+public class LockDemo2 {
+
+   private final static ReentrantReadWriteLock lock=new ReentrantReadWriteLock();
+
+    /**
+     * 悲观锁.
+     */
+   private final static Lock readLock=lock.readLock();
+   private final static Lock writeLock=lock.writeLock();
+
+   private final static Map<String,Data> map=new TreeMap<>();
+
+   public Data getData(String key){
+       readLock.lock();
+       try {
+           return map.get(key);
+       }finally {
+           readLock.unlock();
+       }
+   }
+
+
+   public Set<String> getKeys(){
+       readLock.lock();
+       try {
+           return map.keySet();
+       }finally {
+           readLock.unlock();
+       }
+   }
+
+   public void put(String key,Data value){
+       writeLock.lock();
+       try{
+           map.put(key,value);
+       }finally {
+           writeLock.unlock();
+       }
+   }
+
+
+   class Data{
+
+   }
+
+
+}
---
+++ src/main/java/com/onyx/concurrency/example/lock/LockDemo4.java
@@ -0,0 +1,63 @@
+package com.onyx.concurrency.example.lock;
+
+import com.onyx.concurrency.annotaions.ThreadSafe;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * 模拟并发测试
+ */
+@ThreadSafe
+public class LockDemo4 {
+
+    /**
+     * 客户端的总数
+     */
+    private static int clientTotal=5000;
+    /**
+     * 线程数
+     */
+    private static int threadTotal=200;
+    //计数
+    private static int count=0;
+
+    private final static StampedLock lock=new StampedLock();
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        Semaphore semaphore = new Semaphore(threadTotal);
+        CountDownLatch downLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            pool.execute(()->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    System.out.println(e.toString());
+                }
+                downLatch.countDown();
+            });
+        }
+        downLatch.await();
+        System.out.println("值是:"+count);
+        pool.shutdown();
+    }
+
+    private  static void add(){
+        long stamp = lock.writeLock();
+        try {
+            count++;
+        }finally {
+            lock.unlockWrite(stamp);
+        }
+    }
+
+}
